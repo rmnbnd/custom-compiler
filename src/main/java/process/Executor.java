e6dd8534718314955d8fc867c55027774d18b365
Roman Bondarchuk <roman.bondarchuk@nixsolutions.com>
2016-08-03 10:31:08 +0300

    Add main function

--- src/main/java/process/Executor.java
+++ src/main/java/process/Executor.java
@@ -7,7 +7,9 @@ import core.SymbolAnalise;
 import core.model.ast.AST;
 import core.model.lex.Token;
 import core.model.symbol.Symbol;
+import org.apache.commons.io.IOUtils;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Queue;
 
@@ -17,6 +19,16 @@ public class Executor {
     private Lexer lexer = new Lexer();
     private StaticAnalise staticAnalise = new StaticAnalise();
     private SymbolAnalise symbolAnalise = new SymbolAnalise();
+
+    public static void main(String[] args) throws IOException {
+        Executor executor = new Executor();
+        executor.execute();
+    }
+
+    public void execute() throws IOException {
+        String input = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("input.txt"));
+        execute(input);
+    }
 
     public void execute(String input) {
         Queue<Token> tokens = lexer.lex(input);
