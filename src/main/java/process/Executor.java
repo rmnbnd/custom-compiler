d52ebb4e08b031e336e4d10e7d61757400d52a5a
Roman Bondarchuk <roman.bondarchuk@nixsolutions.com>
2016-08-03 13:56:25 +0300

    Add console program

--- src/main/java/process/Executor.java
+++ src/main/java/process/Executor.java
@@ -7,33 +7,67 @@ import core.SymbolAnalise;
 import core.model.ast.AST;
 import core.model.lex.Token;
 import core.model.symbol.Symbol;
-import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Queue;
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+import static java.lang.System.in;
+import static java.lang.System.out;
 
 public class Executor {
+
+    private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
     private Parser parser = new Parser();
     private Lexer lexer = new Lexer();
     private StaticAnalise staticAnalise = new StaticAnalise();
     private SymbolAnalise symbolAnalise = new SymbolAnalise();
+    private static Executor executor = new Executor();
 
     public static void main(String[] args) throws IOException {
-        Executor executor = new Executor();
         executor.execute();
     }
 
-    public void execute() throws IOException {
-        String input = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("input.txt"));
-        execute(input);
+    public void execute() {
+        String inputString = executor.getInputString();
+        boolean result = executor.execute(inputString);
+        while (!result) {
+            inputString = executor.getInputString();
+            result = executor.execute(inputString);
+        }
     }
 
-    public void execute(String input) {
-        Queue<Token> tokens = lexer.lex(input);
-        AST ast = parser.doParse(tokens);
-        staticAnalise.analis(ast);
-        List<Symbol> symbols = symbolAnalise.analis(ast);
+    public boolean execute(String input) {
+        try {
+            Queue<Token> tokens = lexer.lex(input);
+            AST ast = parser.doParse(tokens);
+            staticAnalise.analis(ast);
+            List<Symbol> symbols = symbolAnalise.analis(ast);
+            return true;
+        } catch (Exception e) {
+            out.println("Error: " + e.getMessage());
+            out.println("Please write code again");
+            return false;
+        }
     }
+
+    private String getInputString() {
+        Scanner scanner = new Scanner(in);
+        StringBuilder stringBuilder = new StringBuilder();
+        while (true) {
+            String input = scanner.nextLine();
+            if ("end".equals(input)) {
+                break;
+            }
+            if ("exit".equals(input)) {
+                exit(0);
+            }
+            stringBuilder.append(input).append(LINE_SEPARATOR);
+        }
+        return stringBuilder.toString();
+    }
+
 }
